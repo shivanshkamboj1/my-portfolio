c637b52d88e1a2c1380117f7b24be5403b6a92db
shivansh <shivansh1221651@jmit.ac.in>
2025-06-16 11:15:39 +0530

    first portfolio

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,42 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">SHIVANSH </span>
+            from <span className="purple"> Karnal, India.</span>
+            <br />
+            I am currently open to internships.
+            <br />
+            I have completed BTECH in Information and Technology from kuk university,
+            Kurukshetra.
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing Games
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Listening songs
+            </li>
+
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "Strive to build things that make a difference!"{" "}
+          </p>
+          <footer className="blockquote-footer">SHIVANSH</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
