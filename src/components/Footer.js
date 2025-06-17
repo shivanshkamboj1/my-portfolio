acfd9b7200d9b3acb4a242e69e679c8bd9569518
shivansh <shivansh1221651@jmit.ac.in>
2025-06-17 14:52:59 +0530

    updated visitor count

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,13 +8,26 @@ import {
 import { FaLinkedinIn } from "react-icons/fa";
 
 function Footer() {
+  const [visitorCount, setVisitorCount] = useState(0);
   let date = new Date();
   let year = date.getFullYear();
+  useEffect(() => {
+    async function fetchVisitorCount() {
+      try {
+        const res = await fetch("https://visitorapis.vercel.app/api/visitor-count");
+        const data = await res.json();
+        setVisitorCount(data.visitorCount || 0);
+      } catch (error) {
+        console.error("Error fetching visitor count:", error);
+      }
+    }
+    fetchVisitorCount();
+  }, []);
   return (
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Thankyou for visiting my portfolio</h3>
+          <h3>Total Visits: {visitorCount}</h3>
         </Col>
         <Col md="4" className="footer-copywright">
           <h3>Copyright © {year}</h3>
